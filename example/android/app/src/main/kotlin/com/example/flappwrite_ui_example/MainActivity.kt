1d1f00ca268f7b5b36f6125ca592bb644dfca4e1
Damodar Lohani <dlohani48@gmail.com>
2022-04-01 20:15:20 +0545

    basic setup

---
+++ example/android/app/src/main/kotlin/com/example/flappwrite_ui_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flappwrite_ui_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/auth/screens/login.screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+import '../views/login.view.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context){
+    return const LoginView();
+  }
+}
